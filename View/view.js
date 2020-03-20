d4c2ebfb866a79f25af85119f4d2868053fd9652
Omer <hbaharkus@gmail.com>
2020-03-20 11:36:06 +0100

    view

--- View/view.js
+++ View/view.js
@@ -3,3 +3,6 @@
     sortedBands
         .map(band => `<li>${band}</li>`)
         .join('');*/
+    document.querySelector('#bands').innerHTML = sortedBands
+    .map(band => `<li>${band}</li>`)
+    .join('');
